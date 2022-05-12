64d126264e34182400945c4b6400a01d6249f084
Hang Nguyen <thuhang.nguyen@relexsolutions.com>
2022-05-13 00:37:15 +0300

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DataConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
