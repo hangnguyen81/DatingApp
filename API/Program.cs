836d0144f0be6b3cf178b491a5a9cf5fed898677
Hang Nguyen <thuhang.nguyen@relexsolutions.com>
2022-07-06 14:07:12 +0300

    end of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DataConnection")));
+builder.Services.AddControllers();
 builder.Services.AddCors();
-
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(options =>
+//     {
+//         options.TokenValidationParameters = new TokenValidationParameters
+//         {
+//             ValidateIssuerSigningKey = true,
+//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(["TokenKey"])),
+//             ValidateIssuer = false,
+//             ValidateAudience = false,
+//         };
+//     });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,10 +34,10 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-//app.UseHttpsRedirection();
-app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+// app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DatingApp
+
 Built with .NET6 and Angular 13
+Tech-stack
+
+- .NET CORE 6 - C#
+- TypeScript
+- Javascript
+- CSS
