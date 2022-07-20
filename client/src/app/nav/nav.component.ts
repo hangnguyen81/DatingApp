d085f441f8e6c9a20ce7d042465cb20620ffcd21
Hang Nguyen <thuhang.nguyen@relexsolutions.com>
2022-07-20 10:31:39 +0300

    section 7 first commit

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
 import { AccountService } from '../services/account.service';
@@ -11,14 +12,14 @@ import { AccountService } from '../services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: (res) => {
-        console.log(res, 'fsdadf');
+        this.router.navigateByUrl('/members');
       },
       error: (error) => console.log(error),
     });
@@ -26,5 +27,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.model = {};
+    this.router.navigateByUrl('/');
   }
 }
